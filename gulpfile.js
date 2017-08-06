982fa629eb972abf151552fc958fb3a423458b06
tetov <sonya.mamurin@gmail.com>
2017-08-06 15:41:33 +0200

    Bootstrap is now installed with npm
    Just use npm install (with projects package.json). main.scss changed to
    point there using loadpath option in gulpfile for sass module.

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,50 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var csscomb = require('gulp-csscomb');
 var gutil = require('gulp-util');
-var imagemin = require('gulp-imagemin');
 var postcss = require('gulp-postcss');
 var run = require('gulp-run');
 var sass = require('gulp-sass');
 var uncss = require('postcss-uncss');
 
+var paths = {
+    source: './_sass/main.scss',
+    bootstrap: './node_modules/bootstrap-sass/assets/stylesheets',
+    build: './_sass/build',
+    html: './_site/**/*.html',
+    dest: './_includes'
+};
+
 // Compiling the CSS from sass
 gulp.task('sass', function () {
-  return gulp.src('./_sass/main.scss')
-	.pipe(sass())
-	.pipe(gulp.dest('./_gulptmp/sass'));
+  return gulp.src( paths.source )
+	.pipe(sass({ includePaths: paths.bootstrap}))
+	.pipe(gulp.dest( paths.build + '/sass' ));
 });
 
 // Sorting the CSS
 gulp.task('comb', ['sass'], function() {
-  return gulp.src('./_gulptmp/sass/main.css')
+  return gulp.src(paths.build + '/sass/main.css')
   .pipe(csscomb())
-  .pipe(gulp.dest('./_gulptmp/comb'));
+  .pipe(gulp.dest( paths.build + '/comb' ));
 });
 
 // Removing unused classes in CSS
 gulp.task('uncss', ['comb'], function() {
     var plugins = [
-        uncss({html: ['./_site/**/*.html']})
-    ]
-    return gulp.src('./_gulptmp/comb/main.css')
-    .pipe(postcss(plugins))
-    .pipe(gulp.dest('./_gulptmp/uncss'));
+        uncss({
+            html: [ paths.html ]
+        }),
+    ];
+    return gulp.src( paths.build + '/comb/main.css' )
+        .pipe(postcss( plugins ))
+        .pipe(gulp.dest( paths.build + '/uncss' ));
 });
 
 // Removing tabs and spaces in CSS
 gulp.task('minify-css', ['uncss'], function() {
-  return gulp.src('./_gulptmp/uncss/main.css')
-  .pipe(cleanCSS({compatibility: 'ie8'}))
-  .pipe(gulp.dest('./_includes'));
+    return gulp.src( paths.build + '/uncss/main.css' )
+    .pipe(cleanCSS())
+    .pipe(gulp.dest( paths.dest ));
 });
 
 // Run all css tasks above in the following fixed sequence
