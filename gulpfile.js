80a9d6ad25b02e4e36c773b981acc67a80ef6b0c
tetov <sonya.mamurin@gmail.com>
2017-08-07 03:16:57 +0200

    Simplified gulpfile with postcss a lot

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var gutil = require('gulp-util');
+var csscomb = require('postcss-csscomb');
+var csswring = require ('csswring');
 var postcss = require('gulp-postcss');
-var run = require('gulp-run');
 var sass = require('gulp-sass');
 var uncss = require('postcss-uncss');
 
@@ -18,35 +16,20 @@ var paths = {
 // Compiling the CSS from sass
 gulp.task('sass', function () {
   return gulp.src( paths.source )
-	.pipe(sass({ includePaths: paths.bootstrap}))
-	.pipe(gulp.dest( paths.build + '/sass' ));
+	.pipe(sass({ includePaths: paths.bootstrap }))
+	.pipe(gulp.dest( paths.build ));
 });
 
-// Sorting the CSS
-gulp.task('comb', ['sass'], function() {
-  return gulp.src(paths.build + '/sass/main.css')
-  .pipe(csscomb())
-  .pipe(gulp.dest( paths.build + '/comb' ));
-});
-
-// Removing unused classes in CSS
-gulp.task('uncss', ['comb'], function() {
+gulp.task('postcss', ['sass'], function() {
     var plugins = [
-        uncss({
-            html: [ paths.html ]
-        }),
+        uncss( { html: [ paths.html ] }),
+        csswring( { removeAllComments: true } ),
+        csscomb()
     ];
-    return gulp.src( paths.build + '/comb/main.css' )
+    return gulp.src( paths.build + '/main.css' )
         .pipe(postcss( plugins ))
-        .pipe(gulp.dest( paths.build + '/uncss' ));
-});
-
-// Removing tabs and spaces in CSS
-gulp.task('minify-css', ['uncss'], function() {
-    return gulp.src( paths.build + '/uncss/main.css' )
-    .pipe(cleanCSS())
-    .pipe(gulp.dest( paths.dest ));
+        .pipe(gulp.dest( paths.dest ));
 });
 
 // Run all css tasks above in the following fixed sequence
-gulp.task('default', ['sass','comb', 'uncss', 'minify-css']);
+gulp.task('default', ['sass', 'postcss']);
