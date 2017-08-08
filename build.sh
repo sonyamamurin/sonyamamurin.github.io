1238ecf6f5ab8a25f06579f7dc23a54a752531a1
tetov <sonya.mamurin@gmail.com>
2017-08-08 23:31:01 +0200

    Comment added to script and added to ignore in _config.yml

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ -d _site ] && [ -f _includes/main.css ] ; then
     exit
 elif [ ! -f _includes/main.css ] ; then
-    touch _includes/main.css
+    touch _includes/main.css # Liquid quits if include not found
 fi
 
 bundle exec jekyll build --trace --strict_front_matter
